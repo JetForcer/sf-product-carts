4a0eb7466e1586f1449558184d2f072f822b4fb5
shumenko@jetforcer.com <Irkmaan1980>
2019-09-07 02:54:39 +0300

    Implement Cart Items checkout logic

--- src/aura/ProductCartPage/ProductCartPageController.js
+++ src/aura/ProductCartPage/ProductCartPageController.js
@@ -3,6 +3,30 @@
         helper.doReloadCartItems(cmp, evt)
     },
 
+    handleCheckout: function (cmp, evt, helper) {
+        var opportunityId = cmp.get('v.recordId');
+        var cartItems = cmp.get('v.cartItems');
+
+        var checkoutAction = cmp.get("c.checkoutCartItems");
+        checkoutAction.setParams({
+            "opportunityId": opportunityId,
+            "cartItems": cartItems
+        });
+
+        checkoutAction.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                helper.doReloadCartItems(cmp, evt);
+                helper.fireCartItemRemoval(opportunityId);
+                $A.get("e.force:closeQuickAction").fire();
+            } else if (state === "ERROR") {
+                alert('Error : ' + JSON.stringify(response.getError()));
+            }
+        });
+
+        $A.enqueueAction(checkoutAction);
+    },
+
     removeFromCart: function (cmp, evt, helper) {
         var opportunityId = cmp.get('v.recordId');
         var product2Id = evt.getParam('row').product2Id;
